03e70b3fad34567a4c2050eb40da58eacc294ca7
mohansai <mohansaikrishna.m20@iiits.in>
2023-12-16 12:48:11 +0530

    frontend codebase

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import TextBar from './components/TextBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      Welcome to EaseWiki!
+      <TextBar />
+    </div>
+  );
+}
+
+export default App;
